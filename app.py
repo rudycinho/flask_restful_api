be0147c61c2abf7b2d0a32cdfbedfd2c7b9189a1
r.martinez1 <r.martinez@empresasim.com>
2020-01-23 10:31:28 -0400

    JWT installed but not update token

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 from flask import Flask,request
 from flask_restful import Resource,Api
+from flask_jwt import JWT,jwt_required
+from security import authenticate,identify
 
 app = Flask(__name__)
+app.secret_key='mark'
 api = Api(app)
+
+jwt = JWT(app,authenticate,identify)
 
 items = []
 
 class Item(Resource):
+    @jwt_required()
     def get(self,name):
         item = next(filter(lambda x:x['name']==name,items),None)
         return {'item':item},200 if item else 404
