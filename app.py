a657269d3fc1a6663141ad28ee7e5f703ad61ba8
r.martinez1 <r.martinez@empresasim.com>
2020-02-07 00:06:04 -0400

    Register a user in ddbb and login

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask,request
 from flask_restful import Resource,Api,reqparse
 from flask_jwt import JWT,jwt_required
+
 from security import authenticate,identify
+from user import UserRegister
 
 app = Flask(__name__)
 app.secret_key='mark'
@@ -58,5 +60,6 @@ class ItemList(Resource):
 
 api.add_resource(Item,'/items/<string:name>')
 api.add_resource(ItemList,'/items/')
+api.add_resource(UserRegister,'/register')
 
 app.run(port=5000,debug=True)
